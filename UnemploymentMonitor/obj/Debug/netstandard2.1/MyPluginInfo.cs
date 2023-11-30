95e265963fa6c973eda096fb13b680b4e4175cdd
Robert Poplin <derangedteddy@gmail.com>
2023-11-29 20:33:29 -0500

    Initial Commit

---
+++ UnemploymentMonitor/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace UnemploymentMonitor
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "UnemploymentMonitor";
+        public const string PLUGIN_NAME = "UnemploymentMonitor";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
